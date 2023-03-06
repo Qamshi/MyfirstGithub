eb3c9b5cbd1b319aef0bc70a391d868210a7af55
Qamrosh maqsood <f200276@cfd.nu.edu.pk>
2023-03-06 20:37:33 +0500

    I added Feedback Page

--- routes/index.route.js
+++ routes/index.route.js
@@ -6,10 +6,15 @@ const router = express.Router();
 const {home} = require("../controller/home");
 const {contact} = require("../controller/contact");
 
+const {About} = require("../controller/About");
+const {Feedback} = require("../controller/Feedback");
+
 
 
 router.get("/home", (req,res)=> home(req,res));
 router.get("/contact", (req,res)=> contact(req,res));
+router.get("/About", (req,res)=> About(req,res));
+router.get("/Feedback", (req,res)=> Feedback(req,res));
 
 
 module.exports=router;
