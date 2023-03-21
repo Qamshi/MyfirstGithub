4e340e22b27556bc94c15751928989bf286d6710
Ayezad yar <f200186@cfd.nu.edu.pk>
2023-03-21 23:33:26 +0500

    customer,order,product

---
+++ model/customermodel.js
@@ -0,0 +1,51 @@
+const {Sequelize , DataTypes } = require ("sequelize");
+
+const sequelize = new Sequelize(
+    'decor',
+    'root',
+    'Mysql123',
+     {
+       host: 'localhost',
+       dialect: 'mysql'
+     }
+   );
+
+ sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+
+
+
+const customer = sequelize.define("customer", {
+   c_id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey : true,
+     autoIncrement: true
+   },
+   c_name: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+   c_email: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   c_address: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   c_password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+
+sequelize.sync().then(() => {
+   console.log('Customer table created successfully!');
+}).catch((error) => {
+   console.error('Unable to create table : ', error);
+});
---
+++ model/productmodel.js
@@ -0,0 +1,53 @@
+const {Sequelize , DataTypes } = require ("sequelize");
+
+const sequelize = new Sequelize(
+    'decor',
+    'root',
+    'Mysql123',
+     {
+       host: 'localhost',
+       dialect: 'mysql'
+     }
+   );
+   
+ sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+
+
+
+const product = sequelize.define("product", {
+   p_id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey : true,
+     autoIncrement: true
+   },
+   p_name: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+   p_desc: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   p_price: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  p_category: {
+   type: DataTypes.STRING,
+   allowNull: false
+ }
+ 
+
+});
+
+
+sequelize.sync().then(() => {
+   console.log('Customer table created successfully!');
+}).catch((error) => {
+   console.error('Unable to create table : ', error);
+});
