502bcd082bebd73331be0c4c8ed9f31d0b22476f
Ayezad yar <f200186@cfd.nu.edu.pk>
2023-04-04 22:51:17 +0500

    pagination and limitation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,27 +1,39 @@
-const { authJwt } = require("../middleware");
-const controller = require("../controller/user.controller");
+// const { authJwt } = require("../middleware");
+// const controller = require("../controller/user.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
+// module.exports = function(app) {
+//   app.use(function(req, res, next) {
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "x-access-token, Origin, Content-Type, Accept"
+//     );
+//     next();
+//   });
 
-  app.get("/api/test/all", controller.allAccess);
+//   app.get("/api/test/all", controller.allAccess);
 
-  app.get(
-    "/api/test/user",
-    [authJwt.verifyToken],
-    controller.userBoard
-  );
+//   app.get(
+//     "/api/test/user",
+//     [authJwt.verifyToken],
+//     controller.userBoard
+//   );
 
+//   app.get(
+//     "/api/test/admin",
+//     [authJwt.verifyToken, authJwt.isAdmin],
+//     controller.adminBoard
+//   );
+// };
 
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
-  );
-};
+const user = require("../controller/user.controller.js");
+
+var router = require("express").Router();
+
+// Create a new Tutorial
+router.post("/", user.create);
+
+// Retrieve all Tutorials
+router.get("/", user.findAll);
+
+module.exports = router;
+
