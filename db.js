f9949cca659fdb21c7e0bc782c5e605185630275
Qamrosh maqsood <f200276@cfd.nu.edu.pk>
2023-04-04 23:19:31 +0500

    Pagination

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize")
 const sequelize = new Sequelize(
-    'decor',
+    'webproject',
     'root',
-    'Mysql123',
+    'Tayyab12.',
      {
        host: 'localhost',
        dialect: 'mysql'
